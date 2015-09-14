54ad5edfa04cdcf8a1e999056ba0d5ae2799939d
Ryan Heupler & Corrie Smith <student@epicodus.com>
2015-09-14 14:46:57 -0700

    adds working index for adding tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(function() {
-  $("form#task").submit(event) {
-    
+  $("form#task").submit(function(event) {
+    event.preventDefault();
+    var inputtedTask = $("input#new-task").val();
+    var description = $("input#new-description").val();
 
-  }
-})
+    var newTask = { task: inputtedTask, newDescription: description };
+
+    $("ul#each-task").append("<li><span class='task'>" + newTask.task + ": " + newTask.newDescription + "</span></li>");
+    $(".task").last().click(function() {
+      $("#each-task").show();
+    });
+  $("input#new-task").val("");
+  $("input#new-description").val("");
+  });
+
+});
